bce749a297b479eca9fb6e496f176d3821d0d5f6
Fang <leanfang@iastate.edu>
2025-02-22 18:57:33 -0600

    case established

--- tests/myRunScript.py
+++ tests/myRunScript.py
@@ -33,3 +33,9 @@ ofm.readField("U", "volVectorField", "2", U)
 UNorm = np.linalg.norm(U)
 UNorm = gcomm.allreduce(UNorm, op=MPI.SUM)
 print("UNorm", UNorm)
+
+if abs(56.45964200732875 - UNorm) / 56.45964200732875 > 1e-6:
+    print("DAPimpleDyMFoam test failed!")
+    exit(1)
+else:
+    print("DAPimpleDyMFoam test passed!")
